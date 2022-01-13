61f3078761b48a5c67b079f3755e5acc6d45c410
Wu Xian Hu <xianhu.wu@ktbhealth.com>
2022-01-13 14:56:27 +0800

    fx

---
+++ javascript-exercise/main.js
@@ -0,0 +1,6 @@
+import {sayHi, sayBye} from './javascript/say.js';
+import {months} from './javascript/say.js';
+
+sayHi('John'); // Hello, John!
+sayBye('John'); // Bye, John!
+console.log("months",months)
