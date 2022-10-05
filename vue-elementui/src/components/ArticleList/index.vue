a12be4a60f49133e9bf6f52c6570102d99ca3ca1
xiaohu <1205150638@qq.com>
2022-10-05 09:00:27 +0800

    fx

---
+++ vue-elementui/src/components/ArticleList/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <el-row>
+    <el-col :span="24" v-for="(o) in 2" :key="o" class="marginBottom10">
+      <el-card class="item">
+        <p class="title">文章标题</p>
+        <p class="desc">文章描述</p>
+        <p class="footer">
+          2021-11-12 09:30
+          <span class="viewer"> <i class="el-icon-view"></i>130 </span>
+        </p>
+      </el-card>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: "ArticleList",
+  props: {},
+}
+
+</script>
+
+
+
+<style scoped>
+.item {
+  text-align: left;
+}
+.title {
+  font-weight: bold;
+}
+.desc {
+  color: #aaa;
+}
+.footer {
+  font-size: 12px;
+}
+.viewer {
+  margin-left: 15px;
+}
+.marginBottom10 {
+  margin-bottom: 10px;
+}
+</style>
+
+
+
---
+++ vue-elementui/src/router/index.vue
@@ -0,0 +1,11 @@
+import Router from 'vue-router'
+import index from '@/components/ArticleList/index.vue'
+
+Vue.use(Router)
+
+export default new Router({
+routes: [
+{ path: '/', redirect: '/login' },
+{ path: '/index', component: index }
+]
+})
