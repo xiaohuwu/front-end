71be19bb3976b9db32b8867ee7593a42bcdb1c7c
xiaohu <1205150638@qq.com>
2022-10-12 15:43:39 +0800

    fx

--- php/hello.php
+++ php/hello.php
@@ -1,8 +1,24 @@
 <?php
-$a = 1;
-$b = 2;
-$c = $a + $b;
-echo $c;
-$car = array("a","b","c");
-echo $car[0];
+//$a = 1;
+//$b = 2;
+//$c = $a + $b;
+//echo $c;
+//$car = array("a","b","c");
+//echo $car[0];
+
+
+$fruits = array (
+    "fruits"  => array("a" => "orange", "b" => "banana", "c" => "apple"),
+    "numbers" => array(1, 2, 3, 4, 5, 6),
+    "holes"   => array("first", 5 => "second", "third")
+);
+
+
+foreach($fruits as $key => $value){
+    echo "key:".$key."  value:".json_encode($value);
+}
+
+//echo json_encode($fruits);
+
+
 ?>
