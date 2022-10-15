4870b85c011c9898954ee699c23e0ca6ecbe3668
xioahuge <xioahuge@xioahugedeMacBook-Pro.local>
2022-10-15 11:10:20 +0800

    fx

--- Vue-exercise/js/test_1.js
+++ Vue-exercise/js/test_1.js
@@ -1,5 +1,42 @@
+Vue.component("task-list", {
+    template: `<div><task v-for="item in tasks">{{item}}</task></div>`,
+    data() {
+        return {
+            tasks: ["java", "ruby", "python"]
+        }
+    }
+});
 
 
 Vue.component('task', {
     template: '<li><slot></slot></li>'
 })
+
+
+Vue.component('message', {
+    props: ['title','body'],
+    data(){
+        return {
+            isShow: true
+        }
+    },
+    methods:{
+        close: function () {
+            this.isShow = false
+        }
+    },
+    template: `
+   <div class="container" v-show="isShow">
+   <div class="panel panel-default">
+    <div class="panel-heading">
+        {{title}} <button @click="close">X</button>
+    </div>
+    <div class="panel-body">
+        {{body}}
+    </div>
+     </div>
+   </div>
+    `
+})
+
+
