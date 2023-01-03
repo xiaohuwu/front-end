9952e80647c89020a707a3c28e6a164a0c8f565f
xiaohu <1205150638@qq.com>
2023-01-04 00:19:21 +0800

    fx

---
+++ my-project/src/components/MyItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <li>
+            <label>
+              <input type="checkbox" />
+              <span>xxxxx</span>
+            </label>
+            <button class="btn btn-danger" style="display: none">删除</button>
+          </li>
+          
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ my-project/src/components/MyList.vue
@@ -0,0 +1,18 @@
+<template>
+   <ul class="todo-main">
+        <MyItem/>
+        <MyItem/>
+    </ul>
+</template>
+
+<script>
+import MyItem from './MyItem.vue';
+
+export default {
+    components: { MyItem }
+}
+</script>
+
+<style>
+
+</style>
