4c0958be436e385284321b970872e5b3b856111e
xiaohu <1205150638@qq.com>
2023-10-20 23:48:35 +0800

    川投签到功能完成

---
+++ vue_test/README.md
@@ -0,0 +1,28 @@
+# vue_test
+
+通过 vue_cli 创建的项目  不成功 删除 npm 缓存。命令行开启代理。
+
+
+# 安装不成功 
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ vue_test/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <h2>一个人的信息</h2>
+  <div>{{name}}</div>
+  <div><button @click="hello">say hello</button></div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import {h} from 'vue';
+export default {
+  name: 'App',
+  setup(){
+    let name = 'Hello World'
+    function hello() {
+      alert(`hello ${name}`)
+    }
+    // return {name,hello}
+    return () => h("h1", "Hello")
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
