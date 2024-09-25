fb15e0f2905e7a56297a8aa0c55cac5ce4391a87
xiaohu <1205150638@qq.com>
2024-09-26 00:58:53 +0800

    first commit

--- php/welcome.php
+++ php/welcome.php
@@ -2,9 +2,39 @@
 
 <body>
     <?php
-    
-    echo 'Hello ' . htmlspecialchars($_POST["name"]) . '!';
+    class Student
+    {
+        var $name;
+        public $age;
+        function __construct()
+        {
+            $this->name = "ddd";
+            $this->age = 0;
+        }
+        function getName()
+        {
+            return $this->name;
+        }
+    }
 
+    $student = new Student();
+    echo $student->getName().PHP_EOL;
+
+
+    function add($student)
+    {
+        $student->age += 5;
+        return $student;
+    }
+    echo add($student)->age.PHP_EOL;
+    function opt($a, $b)
+    {
+        return $a + $b;
+    }
+    echo opt(3,4).PHP_EOL;
+
+    file_put_contents("sites.txt","Runoob");
+    
     ?>
 </body>
 
